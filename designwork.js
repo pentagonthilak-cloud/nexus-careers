1d798c3187d88426af37b0ff0583ddeb74b98e84
pentagonthilak-cloud <pentagonthilak@gmail.com>
2026-06-18 11:32:22 +0530

    Add files via upload

---
+++ designwork.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".apply-btn").forEach(btn => {
+
+btn.addEventListener("click", () => {
+alert("Application Form Opens");
+});
+
+});
+
+document.querySelectorAll(".view-btn").forEach(btn => {
+
+btn.addEventListener("click", () => {
+alert("Job Details Page Opens");
+});
+
+});
